73b3dcdab23685b47c38a273bdabb1e4b1ab242d
isskor <jphuong91@gmail.com>
2021-02-04 18:23:20 +0100

    faq sec

---
+++ src/components/FaqSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How do i start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi totam
+            natus officiis incidunt soluta vel fugit mollitia? Distinctio,
+            consectetur et!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>How do i start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi totam
+            natus officiis incidunt soluta vel fugit mollitia? Distinctio,
+            consectetur et!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>How do i start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi totam
+            natus officiis incidunt soluta vel fugit mollitia? Distinctio,
+            consectetur et!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
